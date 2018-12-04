a781aa6cdad7548abe4d879dfdf3bb6f6ad321dc
Talgat Medetbekov <medetbekov.t@gmail.com>
2018-12-04 15:03:22 +0600

    Cleanup assigning paymentAddressParser in BitcoinKit

--- bitcoinkit/src/main/kotlin/io/horizontalsystems/bitcoinkit/BitcoinKit.kt
+++ bitcoinkit/src/main/kotlin/io/horizontalsystems/bitcoinkit/BitcoinKit.kt
@@ -82,20 +82,19 @@ class BitcoinKit(words: List<String>, networkType: NetworkType, peerSize: Int = 
         peerGroup.transactionSyncer = TransactionSyncer(realmFactory, transactionProcessor, addressManager, bloomFilterManager)
         peerGroup.lastBlockHeightListener = progressSyncer
 
-        paymentAddressParser = when(networkType) {
-            NetworkType.MainNet -> PaymentAddressParser(validScheme = "bitcoin", removeScheme = true)
-            NetworkType.TestNet -> PaymentAddressParser(validScheme = "bitcoin", removeScheme = true)
-            NetworkType.RegTest -> PaymentAddressParser(validScheme = "bitcoin", removeScheme = true)
-            NetworkType.MainNetBitCash -> PaymentAddressParser(validScheme = "bitcoincash", removeScheme = false)
-            NetworkType.TestNetBitCash -> PaymentAddressParser(validScheme = "bitcoincash", removeScheme = false)
-        }
-
-        val addressSelector = when (networkType) {
+        val addressSelector: IAddressSelector
+        when (networkType) {
             NetworkType.MainNet,
             NetworkType.TestNet,
-            NetworkType.RegTest -> BitcoinAddressSelector(addressConverter)
+            NetworkType.RegTest -> {
+                addressSelector = BitcoinAddressSelector(addressConverter)
+                paymentAddressParser = PaymentAddressParser("bitcoin", removeScheme = true)
+            }
             NetworkType.MainNetBitCash,
-            NetworkType.TestNetBitCash -> BitcoinCashAddressSelector(addressConverter)
+            NetworkType.TestNetBitCash -> {
+                addressSelector = BitcoinCashAddressSelector(addressConverter)
+                paymentAddressParser = PaymentAddressParser("bitcoincash", removeScheme = false)
+            }
         }
 
         val stateManager = StateManager(realmFactory, network)
