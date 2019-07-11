6f0f9c9701fc9ed842332c18200c56c1cc45c3ec
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-07-11 10:32:14 +0600

    Remove toString() call, while logging message

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/network/peer/PeerConnection.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/network/peer/PeerConnection.kt
@@ -64,7 +64,7 @@ class PeerConnection(
                 while (isRunning && inStream.available() > 0) {
                     val inputStream = BitcoinInput(inStream)
                     val parsedMsg = networkMessageParser.parseMessage(inputStream)
-                    Timber.i("<= %s", parsedMsg.toString())
+                    Timber.i("<= %s", parsedMsg)
                     listener.onMessage(parsedMsg)
                 }
             }
@@ -106,7 +106,7 @@ class PeerConnection(
     fun sendMessage(message: IMessage) {
         sendingExecutor.execute {
             try {
-                logger.i("=> %s", message.toString())
+                logger.i("=> %s", message)
                 outputStream?.write(networkMessageSerializer.serialize(message))
             } catch (e: Exception) {
                 close(e)
