ec9ee96c828fe2427ded191a89da5ecd702c8add
bakyt <abdrasulov@gmail.com>
2019-10-09 14:31:47 +0600

    Parsing null output data in PluginManager

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/core/PluginManager.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/core/PluginManager.kt
@@ -3,30 +3,42 @@ package io.horizontalsystems.bitcoincore.core
 import io.horizontalsystems.bitcoincore.models.TransactionOutput
 import io.horizontalsystems.bitcoincore.storage.FullTransaction
 import io.horizontalsystems.bitcoincore.transactions.builder.MutableTransaction
+import io.horizontalsystems.bitcoincore.transactions.scripts.Script
 import io.horizontalsystems.bitcoincore.utils.IAddressConverter
 
 class PluginManager(private val addressConverter: IAddressConverter, val storage: IStorage) {
-    private val plugins = mutableListOf<IPlugin>()
+    private val plugins = mutableMapOf<Int, IPlugin>()
 
     fun processOutputs(mutableTransaction: MutableTransaction, extraData: Map<String, Map<String, Any>>) {
         plugins.forEach {
-            it.processOutputs(mutableTransaction, extraData, addressConverter)
+            it.value.processOutputs(mutableTransaction, extraData, addressConverter)
         }
     }
 
     fun addPlugin(plugin: IPlugin) {
-        plugins.add(plugin)
+        plugins[plugin.id] = plugin
     }
 
     fun processTransactionWithNullData(transaction: FullTransaction, nullDataOutput: TransactionOutput) {
-        plugins.forEach {
-            it.processTransactionWithNullData(transaction, nullDataOutput, storage)
+        val script = Script(nullDataOutput.lockingScript)
+        val nullDataChunksIterator = script.chunks.iterator()
+
+        // the first byte OP_RETURN
+        nullDataChunksIterator.next()
+
+        while (nullDataChunksIterator.hasNext()) {
+            val pluginId = nullDataChunksIterator.next()
+            val plugin = plugins[pluginId.opcode] ?: break
+
+            plugin.processTransactionWithNullData(transaction, nullDataChunksIterator, storage)
         }
     }
 
 }
 
 interface IPlugin {
+    val id: Int
+
     fun processOutputs(mutableTransaction: MutableTransaction, extraData: Map<String, Map<String, Any>>, addressConverter: IAddressConverter)
-    fun processTransactionWithNullData(transaction: FullTransaction, nullDataOutput: TransactionOutput, storage: IStorage)
+    fun processTransactionWithNullData(transaction: FullTransaction, nullDataChunks: Iterator<Script.Chunk>, storage: IStorage)
 }
