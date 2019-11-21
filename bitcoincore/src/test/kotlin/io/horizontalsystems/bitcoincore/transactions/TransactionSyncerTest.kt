19f9619a6cbc34850eea89206e3a3ac1961cc795
chyngyz <chyngyz.omurov@gmail.com>
2019-11-21 11:18:10 +0600

    Fix tests in TransactionSyncer

--- bitcoincore/src/test/kotlin/io/horizontalsystems/bitcoincore/transactions/TransactionSyncerTest.kt
+++ bitcoincore/src/test/kotlin/io/horizontalsystems/bitcoincore/transactions/TransactionSyncerTest.kt
@@ -4,10 +4,8 @@ import com.nhaarman.mockitokotlin2.*
 import io.horizontalsystems.bitcoincore.core.IStorage
 import io.horizontalsystems.bitcoincore.managers.BloomFilterManager
 import io.horizontalsystems.bitcoincore.managers.PublicKeyManager
-import io.horizontalsystems.bitcoincore.models.SentTransaction
 import io.horizontalsystems.bitcoincore.models.Transaction
 import io.horizontalsystems.bitcoincore.storage.FullTransaction
-import org.junit.Assert.assertEquals
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.reset
 import org.spekframework.spek2.Spek
@@ -22,10 +20,6 @@ object TransactionSyncerTest : Spek({
 
     val fullTransaction = mock(FullTransaction::class.java)
     val transaction = mock(Transaction::class.java)
-
-    val maxRetriesCount = 3
-    val retriesPeriod: Long = 60 * 1000
-    val totalRetriesPeriod: Long = 60 * 60 * 24 * 1000
 
     beforeEachTest {
         whenever(transaction.hash).thenReturn(byteArrayOf(1, 2, 3))
@@ -76,139 +70,4 @@ object TransactionSyncerTest : Spek({
         }
     }
 
-    describe("#handleTransaction (sent transaction)") {
-
-        context("when SentTransaction does not exist") {
-            beforeEach {
-                whenever(storage.getNewTransaction(transaction.hash)).thenReturn(transaction)
-                whenever(storage.getSentTransaction(transaction.hash)).thenReturn(null)
-            }
-
-            it("adds new SentTransaction object") {
-                argumentCaptor<SentTransaction>().apply {
-                    syncer.handleTransaction(fullTransaction)
-
-                    verify(storage).addSentTransaction(capture())
-                    firstValue.hash = transaction.hash
-                }
-            }
-        }
-
-        context("when SentTransaction exists") {
-            val sentTransaction = SentTransaction()
-
-            beforeEach {
-                sentTransaction.apply {
-                    hash = transaction.hash
-                    firstSendTime = sentTransaction.firstSendTime - 100
-                    lastSendTime = sentTransaction.lastSendTime - 100
-                }
-
-                whenever(storage.getNewTransaction(transaction.hash)).thenReturn(transaction)
-                whenever(storage.getSentTransaction(transaction.hash)).thenReturn(sentTransaction)
-            }
-
-            it("updates existing SentTransaction object") {
-                argumentCaptor<SentTransaction>().apply {
-                    syncer.handleTransaction(fullTransaction)
-
-                    verify(storage).updateSentTransaction(capture())
-                    firstValue.hash = transaction.hash
-                }
-            }
-        }
-
-        context("when Transaction doesn't exist") {
-            beforeEach {
-                whenever(storage.getNewTransaction(transaction.hash)).thenReturn(null)
-            }
-
-            it("neither adds new nor updates existing") {
-                syncer.handleTransaction(fullTransaction)
-
-                verify(storage, never()).addSentTransaction(any())
-                verify(storage, never()).updateSentTransaction(any())
-            }
-        }
-    }
-
-    describe("#getPendingTransactions") {
-
-        context("when transaction is :NEW") {
-            beforeEach {
-                whenever(storage.getNewTransactions()).thenReturn(listOf(fullTransaction))
-            }
-
-            context("when it wasn't sent") {
-                beforeEach {
-                    whenever(storage.getSentTransaction(transaction.hash)).thenReturn(null)
-                }
-
-                it("returns transaction") {
-                    val transactions = syncer.getPendingTransactions()
-
-                    assertEquals(1, transactions.size)
-                    assertEquals(transaction, transactions[0].header)
-                }
-
-            }
-
-            context("when it was sent") {
-                val sentTransaction = SentTransaction()
-
-                beforeEach {
-                    sentTransaction.apply {
-                        hash = transaction.hash
-                        lastSendTime = System.currentTimeMillis() - retriesPeriod - 1
-                        retriesCount = 0
-                    }
-
-                    whenever(storage.getSentTransaction(transaction.hash)).thenReturn(sentTransaction)
-                }
-
-                context("when sent not too many times or too frequently") {
-                    it("returns transaction") {
-                        val transactions = syncer.getPendingTransactions()
-
-                        assertEquals(1, transactions.size)
-                        assertEquals(transaction.hash, transactions[0].header.hash)
-                    }
-                }
-
-                context("when sent too many times") {
-                    it("doesn't return any transaction") {
-                        sentTransaction.retriesCount = maxRetriesCount
-
-                        assertEquals(0, syncer.getPendingTransactions().size)
-                    }
-                }
-
-                context("when sent too often") {
-                    it("doesn't return any transaction") {
-                        sentTransaction.lastSendTime = System.currentTimeMillis()
-
-                        assertEquals(0, syncer.getPendingTransactions().size)
-                    }
-                }
-
-                context("when sent too often in totalRetriesPeriod period") {
-                    it("doesn't return any transaction") {
-                        sentTransaction.firstSendTime = System.currentTimeMillis() - totalRetriesPeriod - 1
-
-                        assertEquals(0, syncer.getPendingTransactions().size)
-                    }
-                }
-            }
-        }
-
-        context("when transaction is not :NEW") {
-            beforeEach {
-                whenever(storage.getNewTransactions()).thenReturn(listOf())
-            }
-
-            it("doesn't return transaction") {
-                assertEquals(0, syncer.getPendingTransactions().size)
-            }
-        }
-    }
 })
