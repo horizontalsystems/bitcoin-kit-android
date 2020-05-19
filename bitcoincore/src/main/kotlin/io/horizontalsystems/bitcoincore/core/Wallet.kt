944034f5f9b0ce46393f30e872e3bf800ac0d0e9
chyngyz <chyngyz.omurov@gmail.com>
2020-05-19 12:40:14 +0600

    Use batch key derivation where possible
    - update hd-wallet-kit
    - add method for batch public key derivation to Wallet

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/core/Wallet.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/core/Wallet.kt
@@ -2,6 +2,7 @@ package io.horizontalsystems.bitcoincore.core
 
 import io.horizontalsystems.bitcoincore.models.PublicKey
 import io.horizontalsystems.hdwalletkit.HDWallet
+import java.lang.Exception
 
 class Wallet(private val hdWallet: HDWallet) {
 
@@ -9,7 +10,24 @@ class Wallet(private val hdWallet: HDWallet) {
 
     fun publicKey(account: Int, index: Int, external: Boolean): PublicKey {
         val hdPubKey = hdWallet.hdPublicKey(account, index, external)
-        return PublicKey(account, index, hdPubKey.external, hdPubKey.publicKey, hdPubKey.publicKeyHash)
+        return PublicKey(account, index, external, hdPubKey.publicKey, hdPubKey.publicKeyHash)
+    }
+
+    fun publicKeys(account: Int, indices: IntRange, external: Boolean): List<PublicKey> {
+        val hdPublicKeys = hdWallet.hdPublicKeys(account, indices, external)
+
+        if (hdPublicKeys.size != indices.count()) {
+            throw HDWalletError.PublicKeysDerivationFailed()
+        }
+
+        return indices.mapIndexed { position, index ->
+            val hdPublicKey = hdPublicKeys[position]
+            PublicKey(account, index, external, hdPublicKey.publicKey, hdPublicKey.publicKeyHash)
+        }
+    }
+
+    open class HDWalletError : Exception() {
+        class PublicKeysDerivationFailed : HDWalletError()
     }
 
 }
