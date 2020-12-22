8f1213edfd1ba6c218b38dec3d377404cb2e3314
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-12-22 17:34:24 +0600

    Add support for two coin types  0 and 145 in BitcoinCashKit

--- bitcoincashkit/src/main/kotlin/io/horizontalsystems/bitcoincash/MainNetBitcoinCash.kt
+++ bitcoincashkit/src/main/kotlin/io/horizontalsystems/bitcoincash/MainNetBitcoinCash.kt
@@ -4,7 +4,7 @@ import io.horizontalsystems.bitcoincore.network.Network
 import io.horizontalsystems.bitcoincore.transactions.scripts.Sighash
 import kotlin.experimental.or
 
-class MainNetBitcoinCash : Network() {
+class MainNetBitcoinCash(coinType: CoinType = CoinType.Type145) : Network() {
 
     override var port: Int = 8333
 
@@ -14,7 +14,7 @@ class MainNetBitcoinCash : Network() {
     override var addressVersion: Int = 0
     override var addressSegwitHrp: String = "bitcoincash"
     override var addressScriptVersion: Int = 5
-    override var coinType: Int = 0
+    override var coinType: Int = coinType.value
 
     override val maxBlockSize = 32 * 1024 * 1024
     override val dustRelayTxFee = 1000 // https://github.com/Bitcoin-ABC/bitcoin-abc/blob/master/src/policy/policy.h#L78
@@ -29,4 +29,9 @@ class MainNetBitcoinCash : Network() {
             "seed.deadalnix.me",                    // Amaury SÉCHET
             "seeder.criptolayer.net"                // criptolayer.net
     )
+
+    enum class CoinType(val value: Int) {
+        Type0(0),
+        Type145(145)
+    }
 }
