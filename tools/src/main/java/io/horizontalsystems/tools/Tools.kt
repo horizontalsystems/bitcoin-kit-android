105c326ba673439dadfde5beebe436ea2e4801bc
LongWen <416441008@qq.com>
2022-06-08 22:36:05 +0800

    更新同步区块

--- tools/src/main/java/io/horizontalsystems/tools/Tools.kt
+++ tools/src/main/java/io/horizontalsystems/tools/Tools.kt
@@ -3,12 +3,11 @@ package io.horizontalsystems.tools
 import io.horizontalsystems.bitcoincore.models.Block
 import io.horizontalsystems.bitcoincore.storage.BlockHeader
 import io.horizontalsystems.bitcoincore.utils.HashUtils
-import java.util.logging.Level
-import java.util.logging.Logger
 
 fun main() {
-    Logger.getLogger("").level = Level.SEVERE
-    syncCheckpoints()
+//    Logger.getLogger("").level = Level.SEVERE
+//    syncCheckpoints()
+    buildCustomCheckpoint()
 }
 
 private fun syncCheckpoints() {
@@ -18,14 +17,14 @@ private fun syncCheckpoints() {
 
 private fun buildCustomCheckpoint() {
     val checkpointBlock = Block(BlockHeader(
-            version = 2,
-            previousBlockHeaderHash = HashUtils.toBytesAsLE("00000000000000006bcf448b771c8f4db4e2ca653474e3b29504ec08422b3fba"),
-            merkleRoot = HashUtils.toBytesAsLE("4ea18e999a57fc55fb390558dbb88a7b9c55c71c7de4cec160c045802ee587d2"),
-            timestamp = 1397755646,
-            bits = 419470732,
-            nonce = 2160181286,
-            hash = HashUtils.toBytesAsLE("00000000000000003decdbb5f3811eab3148fbc29d3610528eb3b50d9ee5723f")
-    ), 296352)
+            version = 536870912,
+            previousBlockHeaderHash = HashUtils.toBytesAsLE("b6d9f1e0e0abfef565e4130572f0ff66f22817689bb20be1c4afd632f36ab21f"),
+            merkleRoot = HashUtils.toBytesAsLE("c466dc598b7f66094557ba1c4939315f258425724d869bcde5356813bf657ef0"),
+            timestamp = 1654694154,
+            bits = 0,
+            nonce = 86431590,
+            hash = HashUtils.toBytesAsLE("546e29c7d3845e897fafca60e2f00e9238e9437a6e1e8e38510921b189f7db12")
+    ), 3932261)
 
     BuildCheckpoints().build(checkpointBlock)
 }
