bd4633be492fbe49d8543a35645cb4145e872923
abdrasulov <abdrasulov@gmail.com>
2023-03-31 14:24:38 +0600

    Fetch all transactions for given address

--- ecashkit/src/main/kotlin/io/horizontalsystems/ecash/ChronikApi.kt
+++ ecashkit/src/main/kotlin/io/horizontalsystems/ecash/ChronikApi.kt
@@ -15,26 +15,35 @@ class ChronikApi : IInitialSyncApi {
 
         for (address in addresses) {
             try {
-                val get = apiManager.get("script/p2pkh/$address/history")
-                val txHistoryPage = Chronik.TxHistoryPage.parseFrom(get)
-                transactionItems.addAll(
-                    txHistoryPage.txsList.map {
-                        TransactionItem(
-                            blockHash = it.block.hash.toByteArray().toReversedHex(),
-                            blockHeight = it.block.height,
-                            txOutputs = it.outputsList.map { txOutput ->
-                                TransactionOutputItem(
-                                    script = txOutput.outputScript.toByteArray().toReversedHex(),
-                                    address = ""
-                                )
-                            }
-                        )
-                    }
-                )
+                var page = 0
+
+                while (true) {
+                    Thread.sleep(10)
+                    val get = apiManager.get("script/p2pkh/$address/history?page=$page")
+                    val txHistoryPage = Chronik.TxHistoryPage.parseFrom(get)
+                    transactionItems.addAll(
+                        txHistoryPage.txsList.map {
+                            TransactionItem(
+                                blockHash = it.block.hash.toByteArray().toReversedHex(),
+                                blockHeight = it.block.height,
+                                txOutputs = it.outputsList.map { txOutput ->
+                                    TransactionOutputItem(
+                                        script = txOutput.outputScript.toByteArray().toReversedHex(),
+                                        address = ""
+                                    )
+                                }
+                            )
+                        }
+                    )
+
+                    page++
+
+                    if (txHistoryPage.numPages < page + 1)
+                        break
+                }
             } catch (e: Throwable) {
                 continue
             }
-            Thread.sleep(10)
         }
 
         return transactionItems
