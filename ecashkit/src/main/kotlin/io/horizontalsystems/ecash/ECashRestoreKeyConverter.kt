13ea2b10a1acd3b8c2710f7c7f1954d192fbe10a
abdrasulov <abdrasulov@gmail.com>
2024-10-21 17:56:51 +0600

    Use blockchair as transactions provider for ecash restore

--- ecashkit/src/main/kotlin/io/horizontalsystems/ecash/ECashRestoreKeyConverter.kt
+++ ecashkit/src/main/kotlin/io/horizontalsystems/ecash/ECashRestoreKeyConverter.kt
@@ -1,12 +1,21 @@
 package io.horizontalsystems.ecash
 
+import io.horizontalsystems.bitcoincore.core.scriptType
 import io.horizontalsystems.bitcoincore.extensions.toHexString
 import io.horizontalsystems.bitcoincore.managers.IRestoreKeyConverter
 import io.horizontalsystems.bitcoincore.models.PublicKey
+import io.horizontalsystems.bitcoincore.utils.AddressConverterChain
+import io.horizontalsystems.hdwalletkit.HDWallet
 
-class ECashRestoreKeyConverter: IRestoreKeyConverter {
+class ECashRestoreKeyConverter(
+    private val addressConverter: AddressConverterChain,
+    private val purpose: HDWallet.Purpose
+) : IRestoreKeyConverter {
     override fun keysForApiRestore(publicKey: PublicKey): List<String> {
-        return listOf(publicKey.publicKeyHash.toHexString())
+        return listOf(
+            publicKey.publicKeyHash.toHexString(),
+            addressConverter.convert(publicKey, purpose.scriptType).stringValue
+        )
     }
 
     override fun bloomFilterElements(publicKey: PublicKey): List<ByteArray> {
